f854a630f07230a2c59fa149a259955d0633ef6f
Pujon Das Auvi <auvipujondas@gmail.com>
2023-11-12 21:12:48 +0600

    navbar updated

--- next.config.js
+++ next.config.js
@@ -1,38 +1,78 @@
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+//    async redirects() {
+//      return [
+//        {
+//          source: '/sign-in',
+//          destination: '/api/auth/login',
+//          permanent: true,
+//        },
+//        {
+//          source: '/sign-up',
+//          destination: '/api/auth/register',
+//          permanent: true,
+//        },
+//      ]
+//    },
+
+//    webpack: (
+//      config,
+//      { buildId, dev, isServer, defaultLoaders, webpack }
+//    ) => {
+//      config.resolve.alias.canvas = false
+//      config.resolve.alias.encoding = false
+//      return config
+//    },
+
+
+//  }
+
+//  module.exports = nextConfig
+
+////  module.exports = {
+////    experimental: {
+////        modern: true,
+////        dynamicImports: true,
+////        css: true
+////    }
+////}
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/sign-in',
-          destination: '/api/auth/login',
-          permanent: true,
-        },
-        {
-          source: '/sign-up',
-          destination: '/api/auth/register',
-          permanent: true,
-        },
-      ]
-    },
+  async redirects() {
+    return [
+      {
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
+      },
+      {
+        source: '/sign-up',
+        destination: '/api/auth/register',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+    ],
+  },
 
-    webpack: (
-      config,
-      { buildId, dev, isServer, defaultLoaders, webpack }
-    ) => {
-      config.resolve.alias.canvas = false
-      config.resolve.alias.encoding = false
-      return config
-    },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
 
-
-  }
-
-  module.exports = nextConfig
-
-//  module.exports = {
-//    experimental: {
-//        modern: true,
-//        dynamicImports: true,
-//        css: true
-//    }
-//}
+module.exports = nextConfig;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,63 +1,70 @@
 import Link from 'next/link'
-import {
-    LoginLink,
-    RegisterLink,
-    getKindeServerSession,
-} from '@kinde-oss/kinde-auth-nextjs/server'
-import WidthWrapper from '@/components/WidthWrapper/WidthWrapper'
+import MaxWidthWrapper from '../WidthWrapper/WidthWrapper'
 import { buttonVariants } from '../ui/button'
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
+import UserAccountNav from '../UserAccountNav/UserAccountNav'
+
+
 
 const Navbar = () => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
+  return (
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+      <MaxWidthWrapper>
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link
+            href='/'
+            className='flex z-40 font-semibold'>
+            <span>quill.</span>
+          </Link>
 
 
-    return (
-        <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/50 backdrop-blur-lg transition-all'>
-            <WidthWrapper>
-                <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
-                    <Link
-                        href='/'
-                        className='flex z-40 font-semibold'>
-                        <span>PDFWhisper.</span>
-                    </Link>
 
-                    <div className='hidden items-center space-x-4 sm:flex'>
+          <div className='hidden items-center space-x-4 sm:flex'>
+            {!user ? (
+              <>
+                <Link
+                  href='/pricing'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: 'sm',
+                  })}>
+                  Get started{' '}
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href='/dashboard'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Dashboard
+                </Link>
 
-                        <>
-                            <Link
-                                href='/pricing'
-                                className={buttonVariants({
-                                    variant: 'ghost',
-                                    size: 'sm',
-                                })}>
-                                Pricing
-                            </Link>
-                            <LoginLink
-                                className={buttonVariants({
-                                    variant: 'ghost',
-                                    size: 'sm',
-                                })}>
-                                Sign in
-                            </LoginLink>
-                            <RegisterLink
-                                className={buttonVariants({
-                                    size: 'sm',
-                                })}>
-                                Get started{' '}
-
-                            </RegisterLink>
-                        </>
-
-                        <>
-                            <Link
-                                href='/dashboard'
-                                className={buttonVariants({
-                                    variant: 'ghost',
-                                    size: 'sm',
-                                })}>
-                                Dashboard
-                            </Link>
-
-                            {/*<UserAccountNav
+                <UserAccountNav
                   name={
                     !user.given_name || !user.family_name
                       ? 'Your Account'
@@ -65,14 +72,14 @@ const Navbar = () => {
                   }
                   email={user.email ?? ''}
                   imageUrl={user.picture ?? ''}
-                />*/}
-                        </>
-
-                    </div>
-                </div>
-            </WidthWrapper>
-        </nav>
-    )
+                />
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
 }
 
 export default Navbar
